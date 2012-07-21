22977ba7a55bb0281c81901fd5c66c41e9dbd0a3
Christopher Sparrowgrove <christopher@sparrowgrove.com>
2012-07-21 03:21:34 -0700

    Inital Upload

--- christopher.sparrowgrove.com/dynamic_pages.php
+++ christopher.sparrowgrove.com/dynamic_pages.php
@@ -16,15 +16,28 @@
  *      
  */
 
-
 /* START CONFIGURATION */
+$apps_directory = "applications/"; //Directory for which will be accessed
+$valid_apps = scandir($apps_directory); //This scans the directory above outputting an array of all apps
+unset($valid_apps[0], $valid_apps[1]);
+//Unset apps you do not want accessed including 0 and 1 for security reasons.
+//You can use print_r($valid_pages); to display the files in the $pages_directory
+
 $pages_directory = "pages/"; //Directory for which will be accessed.
 $valid_pages = scandir($pages_directory); //This scans the directory above outputing an array of all files
 unset($valid_pages[0], $valid_pages[1]); 
 //Unset files you do not want accessed including 0 and 1 for security reasons.
 //You can use print_r($valid_pages); to display the files in the $pages_directory        
 /* END CONFIGURATION */
-
+if (isset($_GET['app']))
+{
+ $app_request = $_GET['app'];
+  if (in_array($app_request.'.php', $valid_apps))
+  {
+   include($apps_directory.''.$app_request."/login.php");
+   exit();
+  }
+}
 if (isset($_GET['page'])) //The Original Code Used !empty but isset is more efficent.
 {
 	$page_request = $_GET['page'];
