c89086b79c6c4f1735554c86033a6a1d0b5028ac
Christopher Sparrowgrove <christopher@sparrowgrove.com>
2012-07-21 03:19:35 -0700

    Addded some HTML

--- christopher.sparrowgrove.com/pages/home.php
+++ christopher.sparrowgrove.com/pages/home.php
@@ -15,6 +15,31 @@
         <meta property="fb:admins" content="100001880332289" />
         <meta name="robots" content="index,follow" />
         <link rel="stylesheet" type="text/css" href="http://christopher.sparrowgrove.com/style.css" />
+        
+        <script type="text/javascript">
+    window._idl = {};
+    _idl.variant = "modal";
+    (function() {
+        var idl = document.createElement('script');
+        idl.type = 'text/javascript';
+        idl.async = true;
+        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+        document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+</script>
+       <script type="text/javascript">
+    window._idl = {};
+    _idl.variant = "banner";
+    (function() {
+        var idl = document.createElement('script');
+        idl.type = 'text/javascript';
+        idl.async = true;
+        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+        document.getElementsByTagName('body')[0].appendChild(idl);
+    })();
+</script>
+
+
  </head> 
  <body>
 	<div id="wrapper">
@@ -31,7 +56,17 @@
         		    </p>
         		   </p>
         		  <hr>
-        		 <p id="adspace2">{Advertise Here}</p>
+        		 <p id="adspace2">
+              <a href="http://internetdefenseleague.org">
+               <img src="http://internetdefenseleague.org/images/badges/final/shield_badge.png" 
+                    width="200" heigth="350" alt="Member of The Internet Defense League" /></a>
+              <BR />
+              <a href="http://github.com/dalyan/sparrowgrove.com/">
+               <img src="http://kinlane-productions.s3.amazonaws.com/github/github-round.png" 
+                    width="200" heigth="350" alt="Get the Code On Github"/>
+               
+              </a>
+             </p>
         		</div>
 			
 			<div id="content">
